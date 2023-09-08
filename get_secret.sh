9fdee888f2a551c59a140854d6a966284c984026
Sean Carolan <sean@carolan.io>
2023-09-08 13:42:50 +0000

    Adds message to script.

--- get_secret.sh
+++ get_secret.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 AWS_REGION=us-west-2
-aws secretsmanager get-secret-value --secret-id '/dev/secrets/captainmidnight' --region $AWS_REGION --query SecretString --output text | jq -r .secretmessage
+echo "Fetching secret message from AWS Secrets Manager..."
+DECODED=$(aws secretsmanager get-secret-value --secret-id '/dev/secrets/captainmidnight' --region $AWS_REGION --query SecretString --output text | jq -r .secretmessage)
+echo $DECODED
